f64e14ef285a461bbb8ab17aa5a10a95cf57f731
Aaron <112088757+AFV9696@users.noreply.github.com>
2022-09-09 08:24:11 -0500

    Cambio 1.0

---
+++ Ejercicio3-9/Account.h
@@ -0,0 +1,33 @@
+// Fig . 3.8 : Account.h
+// Account class with name and balance data members , and a
+// constructor and deposit function that each perform validation .
+#include <string>
+
+
+   class Account {
+      public :
+         Account ( std::string accountName , int initialBalance)
+            : name{accountName} {
+               if(initialBalance > 0 ) {
+                  balance = initialBalance;
+               }
+            }
+         void deposit ( int depositAmount ) {
+            if( depositAmount > 0 ) {
+               balance = balance + depositAmount;
+            }
+         }
+         int getBalance () const {
+            return balance ;
+         }
+         void setName (std::string accountName) {
+            name = accountName ;
+         }
+
+         std :: string getName() const {
+            return name;
+         }
+      private:
+         std :: string name ;
+         int balance {0} ;
+   };
